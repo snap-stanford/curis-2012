eaf9dff3d1abaff84e9ecaaabf2d041c279ddba3
chantat <c.eksombatchai@gmail.com>
2012-06-29 11:10:05 -0700

    My test

--- test_nifty/doc_test.cpp
+++ test_nifty/doc_test.cpp
@@ -1,15 +1,20 @@
 #include "stdafx.h"
 
 int main(int argc, char *argv[]) {
-  TDocBase *DocBase = new TDocBase();
+  TDocBase DocBase;
   TChA Url = TChA("http://www.newyorktimes.com/news_story");
   TSecTm Date = TSecTm::GetCurTm();
   TChA Content = TChA("foo bar foo foo");
-  TVec<TChA> Links = TVec<TChA>();
+  TVec<TChA> Links;
   Links.Add(TChA("http://www.google.com"));
   Links.Add(TChA("http://www.yahoo.com"));
   
-  DocBase->AddDoc(Url, Date, Content, Links);
-  printf("Number of documents: %d", DocBase->GetNumDocs().Val);
+  printf("%d\n", DocBase.AddDoc(Url, Date, Content, Links));
+  printf("Number of documents: %d\n", DocBase.GetNumDocs().Val);
+  
+  TDoc *t = DocBase.GetDoc(0);
+  printf("%s\n", t->GetUrl().CStr());
+  
+  printf("%s\n", t->GetLinks()[0].CStr());
   return 0;
 }
