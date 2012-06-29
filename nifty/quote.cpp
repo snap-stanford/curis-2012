1a1331eb0fb7758411dac61c45b31d22d8d882f5
Caroline Suen <cysuen@stanford.edu>
2012-06-29 13:35:25 -0700

    changed quote.cpp/h to standardize with docs.cpp/h (which needs THashes to function?)

--- nifty/quote.cpp
+++ nifty/quote.cpp
@@ -1,18 +1,35 @@
 #include "stdafx.h"
 #include "quote.h"
+#include "resources/stemming.h"
+//#include "../../snap/glib-core/stemming.h"
 
 TQuote::TQuote(TInt Id, TStrV& Content) {
   this->Id = Id;
   this->Content = Content; // TODO: deep copy?
+  this->ParsedContent = TQuote::StemAndStopWordsContentString(Content);
 }
 
 TQuote::TQuote(TInt Id, TStr ContentString) {
   this->Id = Id;
 	Content = TQuote::ParseContentString(ContentString);
+	ParsedContent = TQuote::StemAndStopWordsContentString(Content);
 }
 
 TStrV TQuote::GetContent() {
   return Content;
+}
+
+TStrV TQuote::GetParsedContent() {
+  return ParsedContent;
+}
+
+TStr TQuote::GetParsedContentString() {
+  TStr ParsedString;
+  for (int i = 0; i < ParsedContent.Len(); ++i) {
+      if (i > 0)  ParsedString.InsStr(ParsedString.Len()," ");
+      ParsedString.InsStr(ParsedString.Len(), ParsedContent[i]);
+  }
+  return ParsedString;
 }
 
 TInt TQuote:: GetId() {
@@ -33,23 +50,34 @@ TStrV TQuote::ParseContentString(TStr ContentString) {
   return ParsedString;
 }
 
+TStrV TQuote::StemAndStopWordsContentString(TStrV ContentV) {
+  /*for (int i = 0; i < ContentV.Len(); ++i) {
+    TStr CurString = ContentV[i];
+    TPorterStemmer TPort;
+    TPorterStemmer::TestMethod();
+    TInt J = 3;
+    TStr test = TPorterStemmer::StemX(CurString);
+    //TStr test = TPorterStemmer::StemX(ContentV[i]);
+    //ContentV[i] = TPorterStemmer::StemX(ContentV[i]);
+  }*/
+  return ContentV;
+}
+
 TQuoteBase::TQuoteBase() {
   QuoteIdCounter = 0;
-  //IdToTQuotes = PHash<TInt, TQuote*>::New();
-  //QuoteToId = PHash<TStrVP, TInt>::New();
 }
 
 /// Adds quote string to quote base; returns quote's quote id
 TQuote* TQuoteBase::AddQuote(TStr ContentString) {
   TStrV ContentVectorString = TQuote::ParseContentString(ContentString);
   TInt QuoteId = GetNewQuoteId(ContentVectorString);
-  if (IdToTQuotes.H.IsKey(QuoteId)) {
-    return IdToTQuotes.H.GetDat(QuoteId); // nothing to do here; quote is already in database
+  if (IdToTQuotes.IsKey(QuoteId)) {
+    return IdToTQuotes.GetDat(QuoteId); // nothing to do here; quote is already in database
   } else {
     // otherwise, create the new TQuote and proceed.
     TQuote* NewQuote = new TQuote(QuoteId, ContentVectorString);
-    //printf("%d: %s\n", QuoteId.Val, ContentString.CStr());
-    IdToTQuotes.H.AddDat(QuoteId, NewQuote);
+    printf("%d: %s\n", QuoteId.Val, NewQuote->GetParsedContentString().CStr());
+    IdToTQuotes.AddDat(QuoteId, NewQuote);
     return NewQuote;
   }
 }
@@ -62,12 +90,12 @@ TQuote* TQuoteBase::AddQuote(TStr ContentString, TInt SourceId) {
 
 void TQuoteBase::RemoveQuote(TInt QuoteId) {
   // TODO: memory management
-  if (IdToTQuotes.H.IsKey(QuoteId)) {
+  if (IdToTQuotes.IsKey(QuoteId)) {
     TQuote* CurQuote = IdToTQuotes.H.GetDat(QuoteId);
-    if (QuoteToId.H.IsKey(CurQuote->GetContent())) {
-      QuoteToId.H.DelKey(CurQuote->GetContent());
+    if (QuoteToId.IsKey(CurQuote->GetContent())) {
+      QuoteToId.DelKey(CurQuote->GetContent());
     }
-    IdToTQuotes.H.DelKey(QuoteId);
+    IdToTQuotes.DelKey(QuoteId);
   }
 }
 
@@ -76,33 +104,33 @@ void TQuoteBase::RemoveQuote(TQuote* Quote) {
 }
 
 TInt TQuoteBase::GetNewQuoteId(TStrV& Content) {
-  if (QuoteToId.H.IsKey(Content)) {
-    return QuoteToId.H.GetDat(Content);
+  if (QuoteToId.IsKey(Content)) {
+    return QuoteToId.GetDat(Content);
   } else {
     // this will increment QuoteIdCounter after returning.
     TInt NewId = QuoteIdCounter;
     QuoteIdCounter++;
-    QuoteToId.H.AddDat(Content, NewId);
+    QuoteToId.AddDat(Content, NewId);
     return NewId;
   }
 }
 
 TInt TQuoteBase::GetQuoteId(TStrV& Content) {
-  if (QuoteToId.H.IsKey(Content)) {
-    return QuoteToId.H.GetDat(Content);
+  if (QuoteToId.IsKey(Content)) {
+    return QuoteToId.GetDat(Content);
   } else {
     return -1;
   }
 }
 
 TQuote* TQuoteBase::GetQuote(TInt QuoteId) {
-  if (IdToTQuotes.H.IsKey(QuoteId)) {
-    return IdToTQuotes.H.GetDat(QuoteId);
+  if (IdToTQuotes.IsKey(QuoteId)) {
+    return IdToTQuotes.GetDat(QuoteId);
   } else {
     return NULL;
   }
 }
 
 int TQuoteBase::Len() {
-  return IdToTQuotes.H.Len();
+  return IdToTQuotes.Len();
 }
