c00c9d1926ae13e6b4e5a605603707c51fef43be
Sandy Huang <shhuang@stanford.edu>
2012-07-25 15:20:37 -0700

    Oops, forgot to add the actual class files in my last commit

---
+++ nifty/lib/incrementalclustering.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "dataloader.h"
+
+void TIncrementalClustering::BuildClusters(TVec<TIntV>& MergedClusters, TVec<TCluster>& ClusterSummaries,
+                                           TQuoteBase& QB, TDocBase& DB, TIntV& NewQuotes) {
+  return;
+}
---
+++ nifty/lib/incrementalclustering.h
@@ -0,0 +1,16 @@
+#ifndef incrementalclustering_h
+#define incrementalclustering_h
+
+#include "../../snap/snap-core/Snap.h"
+#include "niftylib.h"
+
+class TIncrementalClustering {
+private:
+
+public:
+  void BuildClusters(TVec<TIntV>& MergedClusters, TVec<TCluster>& ClusterSummaries,
+                     TQuoteBase& QB, TDocBase& DB, TIntV& NewQuotes);
+
+};
+
+#endif
