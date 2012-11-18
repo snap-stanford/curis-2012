81f0ab90ccc32540de33b16a5cf0e4b2f6ada9a2
Caroline Suen <cysuen@stanford.edu>
2012-11-18 04:00:47 -0800

    memeseed

--- nifty/memeseed.cpp
+++ nifty/memeseed.cpp
@@ -76,6 +76,7 @@ int main(int argc, char *argv[]) {
 
   TIntV Temp;
   //Log.LogAllInformation(&DB, &QB, &CB, TopFilteredClusters, PresentTime, Temp, QBDBCDirectory);
+  Log.WriteClusteringStatisticsToFile(PresentTime);
 
   TStr Directory;
   Log.GetDirectory(Directory);
