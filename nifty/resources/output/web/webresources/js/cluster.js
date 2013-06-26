0eab48cb35e5ddea055f3818f1a57a3c59160e6d
Caroline Suen <cysuen@stanford.edu>
2013-06-26 13:23:09 -0700

    final commit (hopefully) before going through everything and cutting stuff out

--- nifty/resources/output/web/webresources/js/cluster.js
+++ nifty/resources/output/web/webresources/js/cluster.js
@@ -49,8 +49,10 @@ function SetupCluster(clusterID) {
 		
 		// Write everything else
 		var id_map = {};
+    var clust_to_index = {};
 		for (var i = 0; i < data.urls.length; ++i) {
 			id_map[data.ids[i]] = data.quotes[i];
+      clust_to_index[data.ids[i]] = i;
 			var tr = document.createElement("tr");
             $(tr).append("<td>" + data.frequencies[i] + "</td>") // rank
             $(tr).append("<td><a href=\"" + data.urls[i] + "\">" + data.quotes[i] + "</a></td>") // quote
@@ -58,15 +60,77 @@ function SetupCluster(clusterID) {
         }
 		
 		$('#cluster-table').html(table);
+    
+    // filter parents
+    /*real_parents = {}; // one or 0 parents per id
+    has_parents = [];
+    root_index = 0;
+    for (var i = 0; i < data.parents.length; ++i) {
+      //real_parents[i] = [];
+      if (data.parents[i].length == 0) {
+        root_index = i;
+        real_parents[i] = -1;
+        has_parents[0] = i; // lame.
+      }
+    }
+    
+    // brute force bwahahaha
+    while (has_parents.length < data.parents.length) {
+      for (var i = 0; i < data.parents.length; ++i) {
+        if (!real_parents.hasOwnProperty(i)) {
+          // check parents
+        }
+        for (var j = 0; j < data.parents[i].length; ++j) {
+          // iterate across all parents
+          if (
+          graph_string += "\"" + id_map[data.parents[i][j]] + "\"";
+          graph_string += " -> ";
+          graph_string += "\"" + data.quotes[i] + "\";";
+        }
+      }
+    }*/
+    
+    // this is horrible.
+    // build edge matrix.
+    edge_matrix = [];
+    for (var i = 0; i < data.parents.length; ++i) {
+      edge_matrix.push([]);
+      for (var j = 0; j < data.parents.length; ++j) {
+        edge_matrix[i].push(0);
+      }
+      for (var j = 0; j < data.parents[i].length; ++j) {
+        edge_matrix[i][clust_to_index[data.parents[i][j]]] = 1;
+      }
+    }
+    
+    to_remove = [];
+    for (var i = 0; i < data.parents.length; ++i) { // layer 0
+      for (var j = 0; j < data.parents[i].length; ++j) { // layer 1
+        j_ind = clust_to_index[data.parents[i][j]];
+        for (var k = j + 1; k < data.parents[i].length; ++k) {
+          k_ind = clust_to_index[data.parents[i][k]];
+          if (edge_matrix[j_ind][k_ind] == 1) { // k is j's parent
+            edge_matrix[i][k_ind] = 0;
+          } else if (edge_matrix[k_ind][j_ind] == 1) { // j is k's parent
+            edge_matrix[i][j_ind] = 0;
+          }
+        }
+      }
+    }
 		
 		// graph viz
     if (typeof data.parents !== 'undefined') {
       var graph_string = "digraph G { graph [ dpi = 66, width=8]; node [shape=box];"
       for (var i = 0; i < data.parents.length; ++i) {
         for (var j = 0; j < data.parents[i].length; ++j) {
-          graph_string += "\"" + id_map[data.parents[i][j]] + "\"";
-          graph_string += " -> ";
-          graph_string += "\"" + data.quotes[i] + "\";";
+          // only add if we didn't kill the edge
+          if (edge_matrix[i][clust_to_index[data.parents[i][j]]] == 1) {
+            graph_string += "\"" + id_map[data.parents[i][j]] + "\"";
+            graph_string += " -> ";
+            graph_string += "\"" + data.quotes[i] + "\";";
+          } else {
+            console.log(data.ids[i] + " - " + id_map[data.parents[i][j]] + " removed.");
+          }
         }
         
       }
